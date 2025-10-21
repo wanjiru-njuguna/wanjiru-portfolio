1117ab539b672675ba646b59b84023bbef950f17
wanjiru-njuguna <missnjuguna19@gmail.com>
2025-10-21 10:24:00 +0300

    working project witha gitignore file.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+ENV_PATH = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(ENV_PATH)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8v688q8*$=ac0f=mo=@t8n+koml7im9+=57531!v^f(!ijllie'
 
